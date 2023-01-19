8e34fc8254f90490d3b6e4e2861c69b781979d68
Marso Mate <marsomate755@gmail.com>
2023-01-19 11:17:05 +0100

    Love

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "Player 1 wins";
-}
-if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").textContent = "Player 2 wins";
-}
-if (randomNumber1 === randomNumber2){
-    document.querySelector("h1").textContent = "Draw";
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(){
+    var randomNumber = Math.floor(Math.random()*100)+1;
+    var score = "Your score " + randomNumber + "%";
+    document.querySelector(".score").innerHTML = score;
 }
